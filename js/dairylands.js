44139941b7cf6fa3adce9e550f5dda789daecba7
srkarthikg <40857718+srkarthikg@users.noreply.github.com>
2019-06-16 21:47:46 +0530

    first commit
    first commit

---
+++ js/dairylands.js
@@ -0,0 +1,12 @@
+ $(document).on('ready', function() {
+
+      $('.intro-section').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: true,
+        autoplay:true,
+        adaptiveHeight: true
+      });
+    });
