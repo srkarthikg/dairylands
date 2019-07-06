2fdd211369029d23e459237afa7da4924aa80890
srkarthikg <40857718+srkarthikg@users.noreply.github.com>
2019-07-06 18:09:51 +0530

    imagescommit

--- js/dairylands.js
+++ js/dairylands.js
@@ -3,10 +3,14 @@
       $('.intro-section').slick({
         dots: true,
         infinite: true,
-        speed: 300,
+        speed: 1000,
         slidesToShow: 1,
+        mobileFirst: true,
         arrows: true,
         autoplay:true,
         adaptiveHeight: true
       });
+      $(window).on('resize', function() {
+        $('.intro-section').slick('resize');
+    });
     });
