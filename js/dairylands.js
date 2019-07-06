db9c2d91b606b1eed6d502379e0d33358fe726a2
srkarthikg <40857718+srkarthikg@users.noreply.github.com>
2019-07-06 19:22:09 +0530

    new

--- js/dairylands.js
+++ js/dairylands.js
@@ -1,6 +1,25 @@
  $(document).on('ready', function() {
+  /*$('div.top').click(function() {
+    $('html, body').animate({
+      scrollTop: $("div.middle").offset().top
+    }, 1000)
+  });*/
+  $('a[href*=#]').bind('click', function(e) {
+    e.preventDefault(); // prevent hard jump, the default behavior
 
-      $('.intro-section').slick({
+    var target = $(this).attr("href"); // Set the target as variable
+
+    // perform animated scrolling by getting top-position of target-element and set it as scroll target
+    $('html, body').stop().animate({
+        scrollTop: $(target).offset().top
+    }, 600, function() {
+        location.hash = target; //attach the hash (#jumptarget) to the pageurl
+    });
+
+    return false;
+});
+}); 
+    $('.intro-section').slick({
         dots: true,
         infinite: true,
         speed: 1000,
